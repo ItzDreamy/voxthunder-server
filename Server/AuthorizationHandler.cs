952c52e0bf15ae1b69a46ce8673aa025ffbd5caa
VladTop46 <mineserver123ab@gmail.com>
2022-03-04 19:32:41 +0900

    Авторизация (набросок)

---
+++ Server/AuthorizationHandler.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoxelTanksServer
+{
+    internal class AuthorizationHandler
+    {
+        private void clientAuthRequest()
+        {
+            DB db = new DB();
+            MySqlCommand myCommand = new MySqlCommand("SELECT Count(*) FROM  admin WHERE login = '" + "логин" + "' AND password = '" + "пароль в мд5" + "'", db.GetConnection());
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable dt = new DataTable();
+            adapter.SelectCommand = myCommand;
+            adapter.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                //отправлять клиенту что авторизация успешна
+            }
+            else
+            {
+                //посылать клиента нахрен т.к авторизация неудачна
+            }
+        }
+    }
+}
